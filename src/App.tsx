e89cf2c0f94344ae705fd9dd62563477f3bcd2d8
Diego Chapedelaine <25375651+diegochappedelaine@users.noreply.github.com>
2021-09-06 10:36:12 +0200

    fetch event/id & events with search

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import "./App.css";
+import { useFetch } from "hooks";
+import { SEARCH_EVENTS_URL, GET_EVENT_BY_ID } from "api/end-points";
+
+const RANDOM_EVENT_ID = "b14a501dcac06e7b08d870fb94b7528f38e0d093";
+const RANDOM_SEARCH = "danse";
 
 const App = () => {
+  const { data: searchData } = useFetch(
+    `${SEARCH_EVENTS_URL}&search=${RANDOM_SEARCH}`
+  );
+  const { data: eventData } = useFetch(`${GET_EVENT_BY_ID}/${RANDOM_EVENT_ID}`);
+
+  console.log(eventData, searchData);
   return (
     <div>
       <h1>Hello world</h1>
