8cfb5511d91fb60fcd3494f63d78c3f1f2701ce6
Diego Chapedelaine <25375651+diegochappedelaine@users.noreply.github.com>
2021-09-06 11:05:05 +0200

    type declaration for both api points

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { useFetch } from "hooks";
 import { SEARCH_EVENTS_URL, GET_EVENT_BY_ID } from "api/end-points";
+import { GetEventByIdQuery, GetEventsWithSearchQuery } from "global.d";
 
 const RANDOM_EVENT_ID = "b14a501dcac06e7b08d870fb94b7528f38e0d093";
 const RANDOM_SEARCH = "danse";
 
 const App = () => {
-  const { data: searchData } = useFetch(
+  const { data: searchData } = useFetch<GetEventsWithSearchQuery>(
     `${SEARCH_EVENTS_URL}&search=${RANDOM_SEARCH}`
   );
-  const { data: eventData } = useFetch(`${GET_EVENT_BY_ID}/${RANDOM_EVENT_ID}`);
+  const { data: eventData } = useFetch<GetEventByIdQuery>(
+    `${GET_EVENT_BY_ID}/${RANDOM_EVENT_ID}`
+  );
 
-  console.log(eventData, searchData);
+  eventData && console.log(eventData);
+  searchData && console.log(searchData);
+
   return (
     <div>
       <h1>Hello world</h1>
---
+++ src/global.d.ts
@@ -0,0 +1,87 @@
+type Link = {
+  href: string;
+  rel: string;
+};
+
+type LatLon = {
+  lat: number;
+  lon: number;
+};
+
+type Cover = {
+  mimetype: string;
+  format: string;
+  url: string;
+  color_summary: string[];
+  filename: string;
+  last_synchronized: Date;
+  width: number;
+  etag: string;
+  id: string;
+  height: number;
+  thumbnail: boolean;
+};
+
+type Record = {
+  id: string;
+  timestamp: Date;
+  size: number;
+  fields: EventFields;
+};
+
+type EventRecord = {
+  links: Link[];
+  record: Record;
+};
+
+export type EventFields = {
+  blind: number;
+  pmr: number;
+  date_end: Date;
+  deaf: number;
+  updated_at: Date;
+  access_type: string;
+  occurrences: string;
+  contact_name: string;
+  address_street: string;
+  id: string;
+  transport: string;
+  category: string;
+  title: string;
+  cover_alt: string;
+  date_start: Date;
+  price_detail: string;
+  access_link: string;
+  lat_lon: LatLon;
+  address_name: string;
+  contact_twitter: string;
+  contact_phone: string;
+  contact_url: string;
+  description: string;
+  tags: string[];
+  contact_mail: string;
+  access_mail: string;
+  lead_text: string;
+  cover_url: string;
+  contact_facebook: string;
+  access_phone: string;
+  cover_credit: string;
+  address_city: string;
+  programs: string;
+  url: string;
+  cover: Cover;
+  price_type: string;
+  date_description: string;
+  address_zipcode: string;
+};
+
+export interface GetEventsWithSearchQuery {
+  total_count: number;
+  links: Link[];
+  records: EventRecord[];
+}
+
+export interface GetEventByIdQuery {
+  links: Link[];
+  record: Record;
+}
