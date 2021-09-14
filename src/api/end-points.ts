5f8723bc766c24ea99b735200152930e22f50898
Diego Chapedelaine <25375651+diegochappedelaine@users.noreply.github.com>
2021-09-14 12:41:34 +0200

    updated sort query to fetch by most recent

--- src/api/end-points.ts
+++ src/api/end-points.ts
@@ -1,11 +1,12 @@
 const API_URL = `${process.env.REACT_APP_API_URL}`;
 
 export const SEARCH_EVENTS_URL =
-  API_URL + `catalog/datasets/que-faire-a-paris-/records/?&sort=updated_at`;
+  API_URL +
+  `catalog/datasets/que-faire-a-paris-/records/?&sort=updated_at%20desc`;
 
 export const GET_EVENT_BY_ID =
   API_URL + "catalog/datasets/que-faire-a-paris-/records";
 
 export const RETREIVE_LAST_EVENT =
   API_URL +
-  "catalog/datasets/que-faire-a-paris-/records/?&rows=1&sort=updated_at";
+  "catalog/datasets/que-faire-a-paris-/records/?&rows=1&sort=updated_at%20desc";
